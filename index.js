f6c4d3d3d05b7680dcb5a77f65e359bdce2907ca
Daniel Neustadter <daniel.neustadter@vazkeft.net>
2025-08-24 14:39:07 -0500

    Initial commit: dannysecdev's Basic JS Calculator

---
+++ index.js
@@ -0,0 +1,22 @@
+// CALCULATOR PROGRAM
+
+const display = document.getElementById('display');
+
+function appendToDisplay(input) {
+    display.value += input;
+}
+
+function clearDisplay() {
+    display.value = '';
+}
+
+function calculate() {
+    try {
+        display.value = eval(display.value);
+    }
+    catch (error) {
+        console.error('Calculation error:', error);
+        display.value = 'Error';
+    }
+}
+
